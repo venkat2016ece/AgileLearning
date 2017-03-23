c950d97c3794fbf28c21347f81df612c0de18944
venkat2016ece <venkat2016ece@gmail.com>
2017-03-24 08:46:32 +1400

    Add files via upload

---
+++ S1_24_3/Methods/MethodsWithNoReturnValue.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package S1_24_3;
+
+/**
+ * @author welcome
+ *
+ *	We are writing code for methods not returning any values.
+ */
+public class MethodsWithNoReturnValue {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		myMethod();
+
+	}
+	
+	// Here void keyword  makes sure that this myMethod() will not return value
+	public static void myMethod(){
+		System.out.println("Methods with No Return Type Example");
+	}
+
+}
