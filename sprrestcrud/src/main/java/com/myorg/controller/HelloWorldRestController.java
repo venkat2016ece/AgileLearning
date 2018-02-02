26fbe2308eb1131c0268cd8f7d4fe817a5dcb0f3
venkat2016ece <venkat2016ece@gmail.com>
2018-02-02 18:41:58 +0530

    Add files via upload

---
+++ sprrestcrud/src/main/java/com/myorg/controller/HelloWorldRestController.java
@@ -0,0 +1,17 @@
+package com.myorg.controller;
+
+import com.myorg.model.Message;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldRestController {
+
+    @RequestMapping("/hello/{name}")
+    public Message message(@PathVariable String name) {
+
+        Message msg = new Message(name, "Hello " + name);
+        return msg;
+    }
+}
---
+++ sprrestcrud/src/main/java/com/myorg/service/UserService.java
@@ -0,0 +1,82 @@
+package com.myorg.service;
+
+import com.myorg.model.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service("userService")
+@Transactional
+public class UserService {
+
+    private static final AtomicLong counter = new AtomicLong();
+
+    private static List<User> users;
+
+    static{
+        users= populateDummyUsers();
+    }
+
+    public List<User> findAllUsers() {
+        return users;
+    }
+
+    public User findById(long id) {
+        for(User user : users){
+            if(user.getId() == id){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User findByName(String name) {
+        for(User user : users){
+            if(user.getName().equalsIgnoreCase(name)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void saveUser(User user) {
+        user.setId(counter.incrementAndGet());
+        users.add(user);
+    }
+
+    public void updateUser(User user) {
+        int index = users.indexOf(user);
+        users.set(index, user);
+    }
+
+    public void deleteUserById(long id) {
+
+        for (Iterator<User> iterator = users.iterator(); iterator.hasNext(); ) {
+            User user = iterator.next();
+            if (user.getId() == id) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public boolean isUserExist(User user) {
+        return findByName(user.getName())!=null;
+    }
+
+    private static List<User> populateDummyUsers(){
+        List<User> users = new ArrayList<User>();
+        users.add(new User(counter.incrementAndGet(),"jana"));
+        users.add(new User(counter.incrementAndGet(),"gana"));
+        users.add(new User(counter.incrementAndGet(),"mana"));
+        users.add(new User(counter.incrementAndGet(), "nation"));
+        return users;
+    }
+
+    public void deleteAllUsers() {
+        users.clear();
+    }
+}
